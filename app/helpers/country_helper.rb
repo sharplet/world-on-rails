dc1f89ea1b2e333630da102b0a76fd6ad9d75db3
Adam Sharp <adsharp@me.com>
2012-11-19 18:49:34 +1100

    Refactor attr/value printing into shared partial

--- app/helpers/country_helper.rb
+++ app/helpers/country_helper.rb
@@ -1,19 +1,2 @@
 module CountryHelper
-  def pretty_attribute(name)
-    case name
-    when /(id|gnp)/i
-      name.upcase
-    else
-      name.humanize
-    end
-  end
-
-  def pretty_value(value)
-    case value
-    when nil, ''
-      '-'
-    else
-      value
-    end
-  end
 end
