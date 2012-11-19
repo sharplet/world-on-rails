e3f3b3b64ad06fb9b046d288a80716fd85ab7425
Adam Sharp <adsharp@me.com>
2012-11-19 18:42:13 +1100

    Implement CountriesController#show

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,4 +2,8 @@ class CountriesController < ApplicationController
   def index
     @countries = Country.order('continent, name')
   end
+
+  def show
+    @country = Country.find(params[:id])
+  end
 end
