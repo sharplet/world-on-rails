f074683d6c17951a9ddbc2643c1480ffac2a9d97
Adam Sharp <adsharp@me.com>
2012-11-24 12:09:52 +1100

    Validate username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :password, :password_confirmation
   has_secure_password
+  validates :username, :uniqueness => true
   validates :password, :on => :create, :presence => true
 end
